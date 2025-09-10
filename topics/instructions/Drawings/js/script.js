70160db00602884d089a660145357bffb2545a65
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-10 15:16:54 -0400

    added some drawings

--- topics/instructions/Drawings/js/script.js
+++ topics/instructions/Drawings/js/script.js
@@ -7,11 +7,13 @@
 
 "use strict";
 
+const { push } = require("expo-router/build/global-state/routing");
+
 /**
  * OH LOOK I DIDN'T DESCRIBE SETUP!!
 */
 function setup() {
-
+    createCanvas(640, 640);
 }
 
 
@@ -19,5 +21,17 @@ function setup() {
  * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
 */
 function draw() {
+    background(150, 150, 150);
+    
+    push(); // the push and the pop keeps things within them constant while everything around them changes!! 
+    fill(255, 0, 0);
+    stroke(255);
+    ellipse(320, 320, 480);
+    pop();
 
+    push();
+    fill("white");
+    noStroke();
+    ellipse(320, 320, 140, 140);
+    pop();
 }
