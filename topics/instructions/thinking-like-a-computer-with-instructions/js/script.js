06e88d27de892e76c7ead5db447bdd2be5a92657
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-10 19:15:44 -0400

    Following the in-class instructions

--- topics/instructions/thinking-like-a-computer-with-instructions/js/script.js
+++ topics/instructions/thinking-like-a-computer-with-instructions/js/script.js
@@ -1,7 +1,8 @@
 /**
  * Thinking like a computer with instructions
- * Pippin Barr
- * 
+ * Aliyah
+ * September 10th, 2025
+ *
  * An ultra simple example of instructions
  */
 
@@ -9,26 +10,35 @@
 
 /**
  * Creates the canvas
-*/
+ */
 function setup() {
-    createCanvas(400, 400);
+  // "setup" is a p5 instruction in order to run our functions - as soon as the JavaScript loads, it will see the "setup" functoin and run it once
+  createCanvas(400, 400);
+  console.log("Hello World!");
 }
-
 
 /**
  * Sets background, draws the eye
-*/
+ */
 function draw() {
-    // The void
-    background("#000000");
+  // after "setup" is run once, the "draw" function will run over and over again - the closing curly bracket of "draw" is the end of the loop, and basically tells the computer to go back to the start of "draw" and run it again
+  // The void
+  background("#0b28ffff");
+  fill("#004484ff"); // you need to call fill before you draw the shape. If you put it under the shape, the color won't apply to that shape. You can even call it twice to change colors between shapes
+  ellipse(200, 200, 300);
+  rect(100, 100, 50, 30);
+  liasFunction();
 
-    // The eye
-    drawEye();
+  // The eye
+  drawEye();
 }
+
+// It's important to be checking the console log (on your browser) to check if there are any errors. It wil tell you what the error is and what line it's on.
+// In order to be able to use the "draw" function, you NEED to have the "createCanvas" function in the setup
 
 /**
  * Draws a creepy void eye
- */
+ 
 function drawEye() {
     // Eyeball
     push();
@@ -44,3 +54,14 @@ function drawEye() {
     ellipse(200, 200, 25);
     pop();
 }
+*/
+
+// You can create your own functions, but you need to make sure that you call them in order for them to run. "Draw" & "setup" are functions that are in the p5 library, so they will run automatically. But if you create your own function, you need to call it in order for it to run. In this case, we called "drawEye" inside the "draw" function, so it will run every time "draw" runs.
+
+function liasFunction() {
+  console.log.apply("This is my function!");
+  fill(0, 121, 255);
+  rect(200, 300, 80, 60);
+}
+
+// When you call your function, you place it inside another function for it to appear. In this case, we called "liasFunction" inside the "draw" function, so it will run every time "draw" runs.
