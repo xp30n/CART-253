d44106477a7a4dc759c434541474d4918b80d650
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-10 14:41:35 -0400

    Finished practice of functions

--- topics/instructions/function-calls/js/script.js
+++ topics/instructions/function-calls/js/script.js
@@ -1,24 +1,21 @@
 /**
- * Title of Project
- * Author Name
+ * Function Calls
+ * Aliyah
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
+ * Working on the function calls examples
  */
 
 "use strict";
 
-/**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
-*/
-function setup() {
 
+function setup() {
+    // once at the beginning of our program
+    createCanvas(640, 480);
+    // createCanvas will create a canvas, which is a rectangle that we can draw in - the parenthesis basically tell the function to do it, using the properties within the parentheses. These values are called arguments (height and width in this case). The semi colon means that this function is done. 
 }
 
-
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
 function draw() {
-
+    // Every frame that our program runs
+    background(0, 150, 255); // basically fills the canvas with the color that is mentioned in the parentheses (RBG)
+    rect(200, 80, 240, 320); // draws a rectangle - The first number is the x coordinate, the second number is the y coordinate, and the last two numbers are the width and the height
 }
