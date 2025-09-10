92c187c6b3477e9a98e5c596bd4ad55f49d29d80
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-10 15:00:53 -0400

    Starting new drawings functions

---
+++ topics/instructions/Drawings/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Drawings
+ * Aliyah
+ * 
+ * Practicing and learning drawing functions in p5
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
