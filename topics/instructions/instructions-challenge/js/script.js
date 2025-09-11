569891024ed9dc2b9e8bc8c88d21510f63590ac8
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-10 20:05:00 -0400

    Finished the background and the foreground

--- topics/instructions/instructions-challenge/js/script.js
+++ topics/instructions/instructions-challenge/js/script.js
@@ -3,23 +3,40 @@
  * Aliyah 
  * September 10th, 2025
  * 
- * This is my submission for the Instructions Challenge! It's actually super cool:3
+ * This is my submission for the Instructions Challenge! It's actually super cool and looks like the night sky, with a house
  * 
  */
 
 "use strict";
 
+
 /**
  * 
 */
 function setup() {
-
+    createCanvas(900, 600);
 }
 
 
-/**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
-*/
+// The drawing in its entirety
 function draw() {
 
+    // The sky
+    background(20, 52, 164);
+
+    // The moon
+    push();
+    noStroke();
+    ellipse(80, 70, 100);
+    fill(20, 52, 164)
+    noStroke();
+    ellipse(95, 70, 80);    
+    pop();
+
+    // The ground
+    push();
+    noStroke();
+    fill(25, 25, 112);
+    rect(200, 500, 900, 600, 50, 15, 10, 5);
+    pop();
 }
