a4fc10e01c11453c99af10dee1b059d494f1c407
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-15 16:16:26 -0400

    Started the Art Jam Assignment

---
+++ topics/instructions/Art Jam/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Art Jam Assignment
+ * Aliyah
+ * 
+ * This is a self portrait.
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(800, 700);
+    background(137, 207, 240);
+}
+
+
+/**
+ * Parts of my drawing;
+*/
+function draw() {
+
+}
