c5736882654f79e13ae0026497a4678ed74b6d0c
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 12:54:17 -0400

    Used variables to make the program change

--- topics/variables/introducing variables/js/script.js
+++ topics/variables/introducing variables/js/script.js
@@ -7,17 +7,23 @@
 
 "use strict";
 
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+* Create a canvas
 */
 function setup() {
-    createCanvas(w, h, [renderer])
+    createCanvas(640, 480)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a circle in the center of the canvas
 */
 function draw() {
+    background(0)
 
+    push();
+    fill(255, 255, 0);
+    noStroke();
+    ellipse(width / 2, height / 2, 100, 100); // here, instead of tring to figure out where the center of the canvas is and trying to place the circle there, you can simply tell the code to place the circle at the center by dividing the width and the height by 2. No matter how many times you change the canvas size, you the circle will always remain in the middle using the variables 'width/height / 2'. 
 }
