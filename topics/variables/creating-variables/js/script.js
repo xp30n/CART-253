9a00b3cd66364737a10eb487a55aa511647bba12
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 13:10:14 -0400

    Created variables and learned how to use them

--- topics/variables/creating-variables/js/script.js
+++ topics/variables/creating-variables/js/script.js
@@ -7,6 +7,17 @@
 
 "use strict";
 
+//this is how we declare and assign a value to variable - when creating a variable, you use the "let" attribute 
+
+let cheeseRed = 255;
+let cheeseGreen = 255;
+let cheeseBlue = 0;
+
+let holeShade = 0;
+let holeSize = 100; 
+let holeX = 140
+let holeY = 175
+
 /**
  * Creates the canvas
 */
@@ -20,12 +31,12 @@ function setup() {
 */
 function draw() {
     // the cheese
-    background(255, 255, 0);
+    background(cheeseRed, cheeseGreen, cheeseBlue);
 
     // the hole
     push();
     noStroke();
-    fill(0);
-    ellipse(140, 175, 180)
+    fill(holeShade);
+    ellipse(holeX, holeY, holeSize); // you can place the variable within the attributes to make your code easier to read, rather than using hard numbers.
     pop();
 }
