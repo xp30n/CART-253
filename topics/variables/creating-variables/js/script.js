f835d36ab63c510915783a6202c373e383f23f81
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 13:00:59 -0400

    Started Creating Variables module

---
+++ topics/variables/creating-variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Creating Variables
+ * Aliyah Renomeron-Ward
+ * 
+ * Experimenting with creating variables
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
