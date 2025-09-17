6036ae9889f33a64c15d53b0ec52c1cc15114776
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 16:45:53 -0400

    Starting the variables challenge!

---
+++ topics/variables/variables-challenge/js/script.js
@@ -0,0 +1,43 @@
+/**
+ * Variables Challenge
+ * Aliyah R.W.!! 
+ * 
+ * My version of the variables challenge 🛸
+ */
+
+"use strict";
+
+// Our friend Mr. Furious
+let mrFurious = {
+  // Position and size
+  x: 200,
+  y: 200,
+  size: 100,
+  // Colour
+  fill: {
+    r: 255,
+    g: 225,
+    b: 225
+  }
+};
+
+/**
+ * Create the canvas
+ */
+function setup() {
+  createCanvas(400, 400);
+}
+
+/**
+ * Draw (and update) Mr. Furious
+ */
+function draw() {
+  background(160, 180, 200);
+  
+  // Draw Mr. Furious as a coloured circle
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
+  pop();
+}
