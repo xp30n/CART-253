09cb0d85ae88e6bc2bbb94254f33ffbef51055fd
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 13:03:20 -0400

    created a hole in a piece of cheese:3

--- topics/variables/creating-variables/js/script.js
+++ topics/variables/creating-variables/js/script.js
@@ -8,16 +8,24 @@
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(480, 480)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a hole in a piece of cheese
 */
 function draw() {
+    // the cheese
+    background(255, 255, 0);
 
+    // the hole
+    push();
+    noStroke();
+    fill(0);
+    ellipse(140, 175, 180)
+    pop();
 }
