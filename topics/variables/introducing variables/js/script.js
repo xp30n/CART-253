146a2dae85207260e1850dc606a63a681d4a7707
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 12:41:34 -0400

    started variables introduction

---
+++ topics/variables/introducing variables/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Introducing Variables
+ * Aliyah Renomeron-Ward
+ * 
+ * Learning what variables are and what they do
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+    createCanvas(w, h, [renderer])
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
