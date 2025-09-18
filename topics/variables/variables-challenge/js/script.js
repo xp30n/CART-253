ebbef0559ce606a33e518085bcf718bb75c31819
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-17 21:40:21 -0400

    Made Mr. Furious put a shirt on. Sheesh.

--- topics/variables/variables-challenge/js/script.js
+++ topics/variables/variables-challenge/js/script.js
@@ -7,7 +7,7 @@
 
 "use strict";
 
-// Our friend Mr. Furious
+// Our friend Mr. Furious' head
 let mrFurious = {
   // Position and size
   x: 200,
@@ -20,6 +20,38 @@ let mrFurious = {
     b: 225
   }
 };
+
+// Mr.Furious' torso
+let mrFuriousTorso = {
+  x: 185,
+  y: 240,
+  width: 30,
+  height: 79
+};
+
+// the top of Mr.Furious' shirt
+let mrFuriousShirt = {
+  // Position and size
+  x: 200,
+  y: 310,
+  size: 90,
+
+  // The color that he chose today
+  fill: {
+    r: 128,
+    g:0,
+    b: 128
+  }
+};
+
+// the rest of Mr. Furious' shirt
+let mrFuriousShirtBtm = {
+  x: 155,
+  y: 315,
+  width: 90,
+  height: 85
+};
+
 
 /**
  * Create the canvas
@@ -40,4 +72,16 @@ function draw() {
   fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
   ellipse(mrFurious.x, mrFurious.y, mrFurious.size);
   pop();
+
+  // Mr. Furious shirt and neck:3
+  push();
+  noStroke();
+  fill(mrFurious.fill.r, mrFurious.fill.g, mrFurious.fill.b);
+  rect(mrFuriousTorso.x, mrFuriousTorso.y, mrFuriousTorso.width, mrFuriousTorso.height);
+  fill(mrFuriousShirt.fill.r, mrFuriousShirt.fill.g, mrFuriousShirt.fill.b);
+  ellipse(mrFuriousShirt.x, mrFuriousShirt.y, mrFuriousShirt.size);
+  rect(mrFuriousShirtBtm.x, mrFuriousShirtBtm.y, mrFuriousShirtBtm.width, mrFuriousShirtBtm.height);
+  pop();
+
+
 }
