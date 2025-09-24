cadf81362fb2ca1a5072c43deb316ab2a666b119
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-24 11:18:59 -0400

    started the movement project

---
+++ topics/variables/movement/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Movement
+ * Aliyah Renomeron-Ward
+ * 
+ * Practicing with movement
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
