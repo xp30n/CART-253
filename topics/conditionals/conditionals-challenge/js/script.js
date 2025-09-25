866cbca9d2f9222bf279af184f395694f5d7bf7b
Aliyah <aliyahrenomeron7@gmail.com>
2025-09-24 21:09:39 -0400

    Added a target circle and changed it's color depending on the overlap of the puck

--- topics/conditionals/conditionals-challenge/js/script.js
+++ topics/conditionals/conditionals-challenge/js/script.js
@@ -10,14 +10,29 @@ const puck = {
   x: 200,
   y: 200,
   size: 100,
-  fill: "#ff0000"
+  fill: "#ff0000",
+  fills: {
+    noOverlap: "#ff0000",
+    overlap: "purple",
+  },
 };
 
 const user = {
   x: undefined, // will be mouseX
   y: undefined, // will be mouseY
   size: 75,
-  fill: "#000000"
+  fill: "#1900ffff",
+};
+
+const target = {
+  x: 100,
+  y: 100,
+  size: 90,
+  fill: "#0096FF",
+  fills: {
+    noOverlap: "#0096ff",
+    overlap: "#DA70D6"
+  }
 };
 
 /**
@@ -29,51 +44,67 @@ function setup() {
 
 // Check for overlap and move the puck based on the position of the user
 function movePuck() {
-
-    // calculatting the distance between the user's and the puck's center
+  // calculatting the distance between the user's and the puck's center
   const d = dist(user.x, user.y, puck.x, puck.y);
 
   // checking to see if they are overlapping
-  const overlap = (d < user.size/2 + puck.size/2);
+  const overlap = d < user.size / 2 + puck.size / 2;
 
-  // if the user is on the left, 
+  // if the user is on the left,
   if (overlap && user.x < puck.x) {
-    puck.x = puck.x +3; // then move the puck to the right
+    puck.x = puck.x + 2; // then move the puck to the right
   }
 
-  //if the user is on the right, 
+  //if the user is on the right,
   if (overlap && user.x > puck.x) {
-    puck.x = puck.x -3; // move the puck to the left
-
+    puck.x = puck.x - 2; // move the puck to the left
   }
 
-  // if the user is on the bottom, 
-   if (overlap && user.y > puck.y) {
-    puck.y = puck.y -3; // move the puck to the top
+  // if the user is on the bottom,
+  if (overlap && user.y > puck.y) {
+    puck.y = puck.y - 2; // move the puck to the top
   }
 
-  // if the user is on the top, 
+  // if the user is on the top,
   if (overlap && user.y < puck.y) {
-    puck.y = puck.y +3; // move the puck to the bottom
+    puck.y = puck.y + 2; // move the puck to the bottom
   }
 }
 
+function checkTarget() {
+    const d = dist(puck.x, puck.y, target.x, target.y);
+
+    const overlap = (d < puck.size/2 + target.size/2);
+
+    if (overlap) {
+        target.fill = target.fills.overlap;
+    }
+    else {
+        target.fill = target.fills.noOverlap;   
+    }
+}
 
 /**
  * Move the user circle, draw the two circles
  */
 function draw() {
   background("#aaaaaa");
-  
+
   // Move user circle
   moveUser();
-  
+
   // Draw the user and puck
   drawUser();
   drawPuck();
 
   // Move the puck
   movePuck();
+
+  // Draw the target circle
+  drawTarget();
+
+  // Check if the target is being overlapped by the puck
+  checkTarget();
 }
 
 /**
@@ -103,5 +134,14 @@ function drawPuck() {
   noStroke();
   fill(puck.fill);
   ellipse(puck.x, puck.y, puck.size);
+  pop();
+}
+
+// Displays the target circle
+function drawTarget() {
+  push();
+  noStroke();
+  fill(target.fill);
+  ellipse(target.x, target.y, target.size);
   pop();
 }
