6da0a9395b94663be39c8c02648e154d073f4d0c
Aliyah <aliyahrenomeron7@gmail.com>
2025-10-22 09:40:44 -0400

    finished understanding how functions with parameters works

--- topics/functions/trisolaris/js/script.js
+++ topics/functions/trisolaris/js/script.js
@@ -1,6 +1,6 @@
 /**
  * Trisolaris
- * Pippin
+ * Aliyah R.W.
  * 
  * Draws the three suns of Trisolaris. Poorly.
  */
@@ -21,40 +21,22 @@ function draw() {
     // Sky blue
     background("#87ceeb");
 
-    drawSun1();
-    drawSun2();
-    drawSun3();
+    drawSun(500, 100, 80);
+    drawSun(350, 180, 200);
+    drawSun(120, 100, 160);
+    drawSun(400, 300, 10);
 }
 
-/**
- * Draws the first sun
- */
-function drawSun1() {
+// The order of the numbers matters, javascript will read them based on the order that you put the variables in
+
+function drawSun(x, y, size) {
     push();
     noStroke();
     fill("#f99736");
-    ellipse(500, 100, 80);
+    ellipse(x, y, size);
     pop();
 }
 
-/**
- * Draws the second sun
- */
-function drawSun2() {
-    push();
-    noStroke();
-    fill("#f99736");
-    ellipse(350, 180, 200);
-    pop();
-}
+// instead of making three draw functions that are similar but with different x, y and sizes, you can just call different three different suns, and give them the right parameters. Because the rest of the information is the same (fill, stroke, push & pop)
 
-/**
- * Draws the third sun
- */
-function drawSun3() {
-    push();
-    noStroke();
-    fill("#f99736");
-    ellipse(120, 100, 160);
-    pop();
-}
+// now we've created a way to shorten our code!! 
