9f4dc5ea1e704937903857ab78edd7fc69c2bb23
Aliyah <aliyahrenomeron7@gmail.com>
2025-10-29 16:35:44 -0400

    started the speech speech project

---
+++ topics/arrays/speech-speech/js/script.js
@@ -0,0 +1,23 @@
+/**
+ * Sequential Arrays
+ * Aliyah R.W.
+ * 
+ * Interactive speech-playing interface
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
