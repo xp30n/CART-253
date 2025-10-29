b0d44e1a99dacf8e45864946b8094cf3dc9e9957
Aliyah <aliyahrenomeron7@gmail.com>
2025-10-29 18:12:28 -0400

    finished flies flies project

--- topics/arrays/flies-flies/js/script.js
+++ topics/arrays/flies-flies/js/script.js
@@ -14,17 +14,20 @@ let flies = [
     {
         x: 100,
         y: 125,
-        size: 10
+        size: 10,
+        buzziness: 4,
     },
     {
         x: 160,
         y: 170,
-        size: 14
+        size: 14,
+        buzziness: 2,
     },
     {
         x: 180,
         y: 50,
-        size: 5
+        size: 5,
+        buzziness: 3,
     }
 ];
 
@@ -44,9 +47,19 @@ function draw() {
 
     // Display each fly in the array
     for (let fly of flies) {
+        moveFly(fly);
         drawFly(fly);
     }
 }
+
+/**
+ * Moves a fly randomly according to its buzziness
+ */
+function moveFly(fly) {
+    fly.x += random(-fly.buzziness, fly.buzziness);
+    fly.y += random(-fly.buzziness, fly.buzziness);
+}
+
 
 /**
  * Draws the provided fly to the canvas
