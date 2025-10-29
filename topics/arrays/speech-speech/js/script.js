1033daa29eeeca18251ff8ae799d4f1552125d72
Aliyah <aliyahrenomeron7@gmail.com>
2025-10-29 17:04:17 -0400

    using arrays to display Am's speech from 'I have no mouth and I must scream"

--- topics/arrays/speech-speech/js/script.js
+++ topics/arrays/speech-speech/js/script.js
@@ -2,22 +2,51 @@
  * Sequential Arrays
  * Aliyah R.W.
  * 
- * Interactive speech-playing interface
+ * Interactive speech-playing interface; Using Am's speech from "I have no mouth and I must scream"
  */
 
 "use strict";
 
+
+// The speech itself
+const speech = ["I alone had no body", "I was in hell", "looking up at heaven", "I was machine", "and you were flesh"];
+
+// which sentence in the speech to display
+let speechIndex = 0;
+
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Create a canvas
 */
 function setup() {
-
+    createCanvas(600, 100);
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Display the current line of speech
 */
 function draw() {
+    background(0);
 
+    let currentLine = speech[speechIndex]; // Displays the current line of the speech
+
+    // Display the line
+    push();
+    fill("red");
+    textSize(32);
+    textAlign(CENTER, CENTER);
+    text(currentLine, width / 2, height / 2);
+    pop();
 }
+
+function mousePressed() {
+
+    // Displays the next sentence on mouse click
+    speechIndex = speechIndex + 1;
+
+    // Handle the end of the speech
+    if (speechIndex >= speech.length) {
+        // start the speech over again
+        speechIndex = 0;
+    }
+};
