11b05b6e0a89cf390e2d49bca0e4d5a36c3dfcba
Aliyah <aliyahrenomeron7@gmail.com>
2025-11-04 14:21:53 -0500

    added the dialogue window for sir Croaksworth and added his speech array

--- topics/mod-jam/js/script.js
+++ topics/mod-jam/js/script.js
@@ -9,7 +9,7 @@
 
 // SIR CROAKSWORTH IN THE FLESH
 let croaksworth = {
-    x: 290,
+    x: -290,
     y: 280,
     w: 240,
     h: 240 ,
@@ -19,9 +19,23 @@ let croaksworth = {
 
 let arrivalStatement = ["At Last..."]
 
-let speech = [""]
+let speech = [
+    "Huzzah! A willing assistant!",
+    "Welcome traveler",
+    "to the sacred bog of Eternal Hunger.",
+    "My name is Sir Croaksworth,",
+    "Devourer of Destinies,",
+    "...and flies.",
+    "But alas,",
+    "I have not eaten in centuries.",
+    "Aid me, traveler!",
+    "Catch these flies, noble stranger,",
+    "and I shall grant you...",
+    "probably something important.",
+    "Thanks!"
+]
 
-// Loads Sir Croaksworth portrait (Taken by his wizard friend)
+// Loads Sir Croaksworth portrait
 let croaksworthImage;
 
 function preload() {
@@ -44,17 +58,29 @@ function draw() {
     // Load the beautiful Sir Croaksworth's portrait
     drawCroaksworth();
 
+    drawDialogueWindow();
+
 }
 
 function drawCroaksworth() {
 
     // Moves Sir Croaksworth across the screen to the desired X position
-    // if (croaksworth.x < croaksworth.targetX) {
-    //     croaksworth.x += croaksworth.speed;
-    // } else [
-    //     croaksworth.x = croaksworth.targetX,
-    // ]
+    if (croaksworth.x < croaksworth.targetX) {
+        croaksworth.x += croaksworth.speed;
+    } else [
+        croaksworth.x = croaksworth.targetX,
+    ]
 
     // Draws Sir Croaksworth
     image(croaksworthImage, croaksworth.x, croaksworth.y, croaksworth.w, croaksworth.h);
+}
+
+// Draws the dialogue window for Sir Croaksworth's speech
+function drawDialogueWindow() {
+    push();
+    fill(0);
+    stroke("white");
+    strokeWeight(5)
+    rect(240, 70, 355, 140);
+    pop();
 }
