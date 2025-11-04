ed7b484d79c17edd35c85627f878ad677aca3625
Aliyah <aliyahrenomeron7@gmail.com>
2025-11-04 14:45:40 -0500

    updated the opening speech

--- topics/mod-jam/js/script.js
+++ topics/mod-jam/js/script.js
@@ -11,7 +11,7 @@ let pixelFont;
 
 // SIR CROAKSWORTH IN THE FLESH
 let croaksworth = {
-    x: -290,
+    x: 290,
     y: 280,
     w: 240,
     h: 240 ,
@@ -19,19 +19,21 @@ let croaksworth = {
     speed: 0.4 // hehe...
 }
 
+
 let speech = [
-    "Huzzah! A willing assistant!",
+    "Alas...I have arrived",
+    "Huzzah!\n A willing assistant!",
     "Welcome traveler",
-    "to the sacred bog of Eternal Hunger.",
-    "My name is Sir Croaksworth,",
+    "to the sacred bog\n of Eternal Hunger.",
+    "My name is\n Sir Croaksworth,",
     "Devourer of Destinies,",
     "...and flies.",
     "But alas,",
-    "I have not eaten in centuries.",
+    "I have not eaten\n in centuries.",
     "Aid me, traveler!",
-    "Catch these flies, noble stranger,",
+    "Catch these flies,\n noble stranger,",
     "and I shall grant you...",
-    "probably something important.",
+    "probably something\n important.",
     "Thanks!"
 ]
 
@@ -61,13 +63,22 @@ function draw() {
     // Load the beautiful Sir Croaksworth's portrait
     drawCroaksworth();
 
+    // Text of instructions for the player
+    textSize(30);
+    fill(255);
+    stroke(0);
+    textFont(pixelFont);
+    strokeWeight(4);
+    text("Press any key to continue!", 265, 50);
+
+    // Draws the window for the text
     drawDialogueWindow();
 
     // Displays Sir Croaksworth opening speech 
     let currentLine = speech[speechIndex]; 
     push();
     fill(255);
-    textSize(25);
+    textSize(35);
     textFont(pixelFont);
     textAlign(CENTER, CENTER);
     text(currentLine, 420, 140);
@@ -77,11 +88,11 @@ function draw() {
 function drawCroaksworth() {
 
     // Moves Sir Croaksworth across the screen to the middle of the canvas
-    if (croaksworth.x < croaksworth.targetX) {
-        croaksworth.x += croaksworth.speed;
-    } else [
-        croaksworth.x = croaksworth.targetX,
-    ]
+    // if (croaksworth.x < croaksworth.targetX) {
+    //     croaksworth.x += croaksworth.speed;
+    // } else [
+    //     croaksworth.x = croaksworth.targetX,
+    // ]
 
     // Draws Sir Croaksworth
     image(croaksworthImage, croaksworth.x, croaksworth.y, croaksworth.w, croaksworth.h);
@@ -98,6 +109,6 @@ function drawDialogueWindow() {
 }
 
 function keyPressed() {
-    // Displays the next sentence on mouse click
+    // Displays the next sentence on key press
     speechIndex = speechIndex + 1;
 }
