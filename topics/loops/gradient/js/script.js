4132a0fadaed5a728d0f5a846ea150f054092fc6
Aliyah <aliyahrenomeron7@gmail.com>
2025-11-05 10:15:10 -0500

    finished the gradient project

--- topics/loops/gradient/js/script.js
+++ topics/loops/gradient/js/script.js
@@ -1,24 +1,32 @@
 /**
- * Title of Project
- * Author Name
+ * Gradient
+ * Aliyah
  * 
- * HOW EMBARRASSING! I HAVE NO DESCRIPTION OF MY PROJECT!
- * PLEASE REMOVE A GRADE FROM MY WORK IF IT'S GRADED!
- */
+ * Draws a gradient out of lines
+ * 
 
 "use strict";
 
 /**
- * OH LOOK I DIDN'T DESCRIBE SETUP!!
+ * Creates the canvas
 */
 function setup() {
-
+    createCanvas(600, 300)
 }
 
 
 /**
- * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+ * Draws a gradient
 */
 function draw() {
+    background(0);
 
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+
+        push();
+        stroke(shade);
+        line(x, 0, x, height);
+        pop();
+    }
 }
