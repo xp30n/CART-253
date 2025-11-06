1a8de59b58cf7e84c10f750617476307f6ee5117
Lia <aliyahrenomeron7@gmail.com>
2025-11-05 20:06:01 -0500

    added the while loop to simplify the code

--- topics/loops/loops-challenge/js/script.js
+++ topics/loops/loops-challenge/js/script.js
@@ -19,37 +19,22 @@ function setup() {
  */
 function draw() {
     background("pink");
-    
+
+    let x = 0;
+    let y = 0;
+    let w = 0;
+    let h = 500;
+    let s = 0;
+
+    while (x <= 500) {
+        stroke(s);
+        line(x, y, w, h);
+        x+= 50;
+        w += 50;
+    }
+
     stroke(0);
-    line(0, 0, 0, height);
-    
-    stroke(25);
-    line(50, 0, 50, height);
-    
-    stroke(50);
-    line(100, 0, 100, height);
-    
-    stroke(75);
-    line(150, 0, 150, height);
-    
-    stroke(100);
-    line(200, 0, 200, height);
-    
-    stroke(125);
-    line(250, 0, 250, height);
-    
-    stroke(150);
-    line(300, 0, 300, height);
-    
-    stroke(175);
-    line(350, 0, 350, height);
-    
-    stroke(200);
-    line(400, 0, 400, height);
-    
-    stroke(225);
-    line(450, 0, 450, height);
-    
-    stroke(250);
-    line(500, 0, 500, height);
+    line(x, y, w, h);
+    x+= 50;
+    w += 50;
 }
