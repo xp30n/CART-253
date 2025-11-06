30eb7b7f0f92e6d18b31ce87569cb40dca1973cc
Lia <aliyahrenomeron7@gmail.com>
2025-11-05 20:46:11 -0500

    added the gradient

--- topics/loops/loops-challenge/js/script.js
+++ topics/loops/loops-challenge/js/script.js
@@ -10,7 +10,7 @@
  * Creates the canvas
  */
 function setup() {
-    createCanvas(500, 500);
+  createCanvas(500, 500);
 }
 
 /**
@@ -18,31 +18,42 @@ function setup() {
  * gradually lightening colour
  */
 function draw() {
-    background("pink");
 
-    let x = 0;
-    let y = 0;
-    let w = 0;
-    let h = 500;
-    let s = 0;
+    drawGradient();
 
-    // vertical lines
-    while (x <= height) {
-        stroke(0);
-        line(x, 0, x, h);
-        x += 50;
-        s += 50; 
-        w += 50;
-    }
+  let x = 0;
+  let y = 0;
+  let w = 0;
+  let h = 500;
+  let s = 0;
 
-    // horizontal lines
-    s = 0;
-    y = 0;
+  // vertical lines
+  while (x <= height) {
+    stroke(0);
+    line(x, 0, x, h);
+    x += 50;
+    s += 50;
+    w += 50;
+  }
 
-    while (y <= height) {
-        stroke(0);
-        line(0, y, w, y);
-        y += 50;
-        s += 50;
-    }
+  // horizontal lines
+  s = 0;
+  y = 0;
+
+  while (y <= height) {
+    stroke(0);
+    line(0, y, w, y);
+    y += 50;
+    s += 50;
+  }
+}
+
+function drawGradient() {
+    for (let x = 0; x <= width; x++) {
+        const shade = map(x, 0, width, 0, 255);
+        push();
+        stroke(shade, shade, 255);
+        line(x, 0, x, height);
+        pop();
+      }
 }
