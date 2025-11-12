bed03fbac6f6e4ad84d83e9f298a87bf2ab366a7
Lia <aliyahrenomeron7@gmail.com>
2025-11-12 12:12:50 -0500

    completed the tarot project

--- topics/data/tarot/js/script.js
+++ topics/data/tarot/js/script.js
@@ -7,17 +7,42 @@
 
 "use strict";
 
-/**
- * tbd.
-*/
-function setup() {
+// our tarot data
+let tarot = undefined;
 
+// Our fortune
+let fortune = "Click to show a fortune";
+
+function preload() {
+    tarot = loadJSON("assets/data/tarot_interpretations.json");
 }
 
+/**
+ * creates a canvas
+*/
+function setup() {
+    createCanvas(800, 400);
+}
 
 /**
- * tbd.
+ * display the tarot
 */
 function draw() {
+    background(0);
 
+    // Display the information
+    push();
+    textSize(16);
+    fill("blue");
+    textAlign(CENTER, CENTER);
+    text(fortune, width / 2, height / 2);
+    pop();
+}
+
+function mousePressed() {
+    // Choose a random card
+    const card = random(tarot.tarot_interpretations);
+
+    // Choose a random fortune
+    fortune = random(card.fortune_telling);
 }
