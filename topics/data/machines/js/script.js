4124359e6eb12c18bb95861b4efe79c0f6d84e37
Lia <aliyahrenomeron7@gmail.com>
2025-11-12 09:58:35 -0500

    completed the machine project

--- topics/data/machines/js/script.js
+++ topics/data/machines/js/script.js
@@ -10,6 +10,30 @@
 
 "use strict";
 
+const machines = [{
+    type: "incinerator", // give the machine a name! 
+    x: 0,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#ff4400"
+}, {
+    type: "freezer",
+    x: 150,
+    y: 100,
+    width: 100,
+    height: 100,
+    fill: "#bbbbff"
+
+}, {
+    type: "crusher",
+    x: 300,
+    y: 100, 
+    width: 100,
+    height: 100,
+    fill: "#777777"
+}];
+
 /**
  * Create the canvas
  */
@@ -23,24 +47,17 @@ function setup() {
 function draw() {
     background(0);
 
-    // Incinerator
-    push();
-    noStroke();
-    fill("#ff4400");
-    rect(0, 100, 100, 100);
-    pop();
+    for (let machine of machines) {
+        drawMachine(machine);
+    }
+}
 
-    // Freezer
+function drawMachine(machine) {
     push();
     noStroke();
-    fill("#bbbbff");
-    rect(150, 100, 100, 100);
-    pop();
-
-    // Crusher
-    push();
-    noStroke();
-    fill("#777777");
-    rect(300, 100, 100, 100);
+    fill(machine.fill);
+    rect(machine.x, machine.y, machine.width, machine.height);
     pop();
 }
+
+// machine is just a placeholder name, it will take whatever value is called within the draw even if there is no machine variable! 
